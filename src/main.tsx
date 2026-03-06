122253710766ef7950bd1acff28413cdc52a9406
HDeveloper <omur48666@gmail.com>
2026-03-06 01:28:44 +0000

    Fix router for Lovable + GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+const isGitHubPages = window.location.hostname.includes("github.io");
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/derpublic-civ-experiment2">
+  <BrowserRouter basename={isGitHubPages ? "/derpublic-civ-experiment2" : undefined}>
     <App />
   </BrowserRouter>
 );
